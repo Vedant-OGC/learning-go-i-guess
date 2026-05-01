ff5fbd5bf4e2fd605c5af5419d09e9cf35eb7f31
Vedant-OGC <sharonverma73@gmail.com>
2026-05-01 14:15:00 +0530

    compiler yelled at me for this

--- basics/types.go
+++ basics/types.go
@@ -10,7 +10,11 @@ func Types() {
 	
 	fmt.Println(a, b, c, d)
 	
-	// type conversions
 	var e int = int(b)
-	fmt.Println("float to int:", e) // loses precision, classic
+	fmt.Println("float to int:", e)
+	
+	// string to int conversion?
+	// val := int("123") // doesn't work like Python!
+	// string(65) gives "A", which is weird
+	fmt.Println("string conversion:", string(rune(65)))
 }
