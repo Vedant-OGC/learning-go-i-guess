1a8f6e6799634f2a1071c861c4a5548f3637d9ab
Vedant-OGC <sharonverma73@gmail.com>
2026-05-02 10:05:00 +0530

    iota is actually cool

--- basics/constants.go
+++ basics/constants.go
@@ -4,7 +4,13 @@ import "fmt"
 
 const Pi = 3.14159
 
+// iota auto increments!
+const (
+	Sunday = iota // 0
+	Monday        // 1
+	Tuesday       // 2
+)
+
 func Constants() {
-	const greeting = "hello constants"
-	fmt.Println(Pi, greeting)
+	fmt.Println(Sunday, Monday, Tuesday)
 }
