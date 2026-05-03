7fee994c525f1f82dd9edcc3d04945758496b9eb
Vedant-OGC <sharonverma73@gmail.com>
2026-05-03 12:15:00 +0530

    loops as while loops

--- control/loops.go
+++ control/loops.go
@@ -3,8 +3,10 @@ package control
 import "fmt"
 
 func Loops() {
-	// go only has for!
-	for i := 0; i < 5; i++ {
-		fmt.Println("i =", i)
+	// for as while
+	count := 0
+	for count < 3 {
+		fmt.Println("while equivalent count:", count)
+		count++
 	}
 }
