5ee81643fe0b88d39beff460fc67f0c585cc0acc
Vedant-OGC <sharonverma73@gmail.com>
2026-05-03 14:50:00 +0530

    infinite loop goes brrr

--- control/loops.go
+++ control/loops.go
@@ -3,10 +3,19 @@ package control
 import "fmt"
 
 func Loops() {
-	// for as while
 	count := 0
 	for count < 3 {
 		fmt.Println("while equivalent count:", count)
 		count++
 	}
+	
+	// infinite loop with break
+	i := 0
+	for {
+		if i == 5 {
+			break
+		}
+		i++
+	}
+	// careful lol
 }
