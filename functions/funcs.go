9e6ff6cb6dc6d3b9dee82ec36254af19e26d11b2
Vedant-OGC <sharonverma73@gmail.com>
2026-05-05 19:10:00 +0530

    blank identifier to ignore returns

--- functions/funcs.go
+++ functions/funcs.go
@@ -2,20 +2,12 @@ package functions
 
 import "fmt"
 
-// multiple return values
+func Funcs() {
+	// using blank identifier to ignore values
+	_, b := swap("hello", "world")
+	fmt.Println("ignored first return:", b)
+}
+
 func swap(x, y string) (string, string) {
 	return y, x
-}
-
-// named return values
-func split(sum int) (x, y int) {
-	x = sum * 4 / 9
-	y = sum - x
-	return // naked return, dangerous but cool
-}
-
-func Funcs() {
-	a, b := swap("hello", "world")
-	fmt.Println(a, b)
-	fmt.Println(split(17))
 }
