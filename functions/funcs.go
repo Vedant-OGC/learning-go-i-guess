443713d7914c605f0a4eba0d816cb7a1705a76a8
Vedant-OGC <sharonverma73@gmail.com>
2026-05-05 12:30:00 +0530

    multiple return values

--- functions/funcs.go
+++ functions/funcs.go
@@ -2,10 +2,20 @@ package functions
 
 import "fmt"
 
-func add(x int, y int) int {
-	return x + y
+// multiple return values
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+// named return values
+func split(sum int) (x, y int) {
+	x = sum * 4 / 9
+	y = sum - x
+	return // naked return, dangerous but cool
 }
 
 func Funcs() {
-	fmt.Println("add:", add(5, 7))
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	fmt.Println(split(17))
 }
