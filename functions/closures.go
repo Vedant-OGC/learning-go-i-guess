06a11994b85392af5f20d7357e979a00fd19f6d6
Vedant-OGC <sharonverma73@gmail.com>
2026-05-06 21:50:00 +0530

    fixed the loop closure bug

--- functions/closures.go
+++ functions/closures.go
@@ -3,11 +3,11 @@ package functions
 import "fmt"
 
 func Closures() {
-	// closure capture bug
 	var funcs []func()
 	for i := 0; i < 4; i++ {
+		iCopy := i
 		funcs = append(funcs, func() {
-			fmt.Println(i) // captured by reference!
+			fmt.Println(iCopy)
 		})
 	}
 	
