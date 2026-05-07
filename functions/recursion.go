bf99798bfb838d774312622ccbef07735d0fb531
Vedant-OGC <sharonverma73@gmail.com>
2026-05-07 15:40:00 +0530

    missing base case crash

--- functions/recursion.go
+++ functions/recursion.go
@@ -2,13 +2,6 @@ package functions
 
 import "fmt"
 
-func fib(n int) int {
-	if n < 2 {
-		return n
-	}
-	return fib(n-1) + fib(n-2)
-}
-
 func Recursion() {
-	fmt.Println(fib(10))
+	fmt.Println("recursion works fine with base case")
 }
