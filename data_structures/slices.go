3dc15ed46cd2eac6454fa3a3b477a48bbca9fcc3
Vedant-OGC <sharonverma73@gmail.com>
2026-05-08 22:15:00 +0530

    slices are references??

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -3,8 +3,15 @@ package datastructures
 import "fmt"
 
 func Slices() {
-	var s []int
-	s = append(s, 1)
-	s = append(s, 2)
-	fmt.Println("appended slice:", s)
+	s := []int{1, 2, 3, 4, 5}
+	
+	// delete element at index 2 (value 3)
+	s = append(s[:2], s[3:]...)
+	fmt.Println("after delete:", s) // [1 2 4 5]
+	
+	// slices are references!
+	original := []int{100, 200, 300}
+	copied := original
+	copied[0] = 999
+	fmt.Println("original:", original[0]) // mutated! cursed
 }
