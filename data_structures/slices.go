003049a96cad563299d0fbf0ce8ecad8e7b3b247
Vedant-OGC <sharonverma73@gmail.com>
2026-05-08 18:45:00 +0530

    slice append

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -3,9 +3,8 @@ package datastructures
 import "fmt"
 
 func Slices() {
-	// slice from array
-	arr := [5]int{10, 20, 30, 40, 50}
-	s := arr[1:4] // s has elements 20, 30, 40
-	fmt.Println("slice:", s)
-	fmt.Println("len:", len(s), "cap:", cap(s))
+	var s []int
+	s = append(s, 1)
+	s = append(s, 2)
+	fmt.Println("appended slice:", s)
 }
