b314b1d715011efa1fe89b0103de4c602479b805
Vedant-OGC <sharonverma73@gmail.com>
2026-05-09 18:30:00 +0530

    checking key existence in maps

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -3,8 +3,9 @@ package datastructures
 import "fmt"
 
 func Maps() {
-	m := make(map[string]int)
-	m["k1"] = 7
-	m["k2"] = 13
-	fmt.Println("map:", m)
+	m := map[string]int{"foo": 1, "bar": 2}
+	
+	// check if key exists
+	v, ok := m["baz"]
+	fmt.Println("value:", v, "exists?:", ok)
 }
