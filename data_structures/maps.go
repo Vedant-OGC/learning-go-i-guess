433769727493ddd32112fcb76550fb2dbbbafe8b
Vedant-OGC <sharonverma73@gmail.com>
2026-05-09 21:50:00 +0530

    deleting keys from map

--- data_structures/maps.go
+++ data_structures/maps.go
@@ -4,8 +4,6 @@ import "fmt"
 
 func Maps() {
 	m := map[string]int{"foo": 1, "bar": 2}
-	
-	// check if key exists
-	v, ok := m["baz"]
-	fmt.Println("value:", v, "exists?:", ok)
+	delete(m, "foo")
+	fmt.Println("after delete:", m)
 }
