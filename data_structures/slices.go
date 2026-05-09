c674abd77e6d642467ce231021f4884388f6d7ed
Vedant-OGC <sharonverma73@gmail.com>
2026-05-09 12:45:00 +0530

    slice copy

--- data_structures/slices.go
+++ data_structures/slices.go
@@ -3,15 +3,11 @@ package datastructures
 import "fmt"
 
 func Slices() {
-	s := []int{1, 2, 3, 4, 5}
-	
-	// delete element at index 2 (value 3)
-	s = append(s[:2], s[3:]...)
-	fmt.Println("after delete:", s) // [1 2 4 5]
-	
-	// slices are references!
 	original := []int{100, 200, 300}
-	copied := original
+	
+	// use copy to prevent mutating original
+	copied := make([]int, len(original))
+	copy(copied, original)
 	copied[0] = 999
-	fmt.Println("original:", original[0]) // mutated! cursed
+	fmt.Println("original (safe):", original[0]) // still 100
 }
