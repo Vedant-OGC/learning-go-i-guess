b1e3a66e49201676241314c733f6a92ae9b1db75
Vedant-OGC <sharonverma73@gmail.com>
2026-05-11 15:00:00 +0530

    pointer methods on structs

--- pointers/pointers_wtf.go
+++ pointers/pointers_wtf.go
@@ -2,6 +2,25 @@ package pointers
 
 import "fmt"
 
+type Counter struct {
+	val int
+}
+
+// value receiver doesn't mutate!
+func (c Counter) AddValue() {
+	c.val++
+}
+
+// pointer receiver mutates!
+func (c *Counter) AddPointer() {
+	c.val++
+}
+
 func PointersWtf() {
-	fmt.Println("nil pointer dereference causes panic")
+	c := Counter{val: 0}
+	c.AddValue()
+	fmt.Println("after value receiver:", c.val) // 0
+	
+	c.AddPointer()
+	fmt.Println("after pointer receiver:", c.val) // 1
 }
