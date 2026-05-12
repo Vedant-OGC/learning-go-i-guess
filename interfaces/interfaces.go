5daee3a08f891160e6427c5fd93d561962c10553
Vedant-OGC <sharonverma73@gmail.com>
2026-05-12 12:50:00 +0530

    implementing shape methods

--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -1,15 +1,24 @@
 package interfaces
 
-import "fmt"
+import (
+	"math"
+)
 
 type Shape interface {
 	Area() float64
+	Perimeter() float64
 }
 
 type Circle struct {
 	Radius float64
 }
 
-func Interfaces() {
-	fmt.Println("interfaces intro")
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
+
+func (c Circle) Perimeter() float64 {
+	return 2 * math.Pi * c.Radius
+}
+
+func Interfaces() {}
