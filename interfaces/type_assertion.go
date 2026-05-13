00fa199f0126d5839ece4960af013e06fa314a31
Vedant-OGC <sharonverma73@gmail.com>
2026-05-13 13:50:00 +0530

    type switch

--- interfaces/type_assertion.go
+++ interfaces/type_assertion.go
@@ -2,11 +2,19 @@ package interfaces
 
 import "fmt"
 
+func do(i interface{}) {
+	switch v := i.(type) {
+	case int:
+		fmt.Printf("Twice %v is %v\n", v, v*2)
+	case string:
+		fmt.Printf("%q is %v bytes long\n", v, len(v))
+	default:
+		fmt.Printf("I don't know about type %T!\n", v)
+	}
+}
+
 func TypeAssertion() {
-	var i interface{} = "hello"
-	s, ok := i.(string)
-	fmt.Println(s, ok)
-	
-	f, ok := i.(float64)
-	fmt.Println(f, ok) // safe, doesn't panic
+	do(21)
+	do("hello")
+	do(true)
 }
