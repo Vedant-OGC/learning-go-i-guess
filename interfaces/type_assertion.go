95c7416a084cb68dc139ea96cce1f35949d2f274
Vedant-OGC <sharonverma73@gmail.com>
2026-05-13 16:15:00 +0530

    empty interface is like python any

--- interfaces/type_assertion.go
+++ interfaces/type_assertion.go
@@ -2,19 +2,8 @@ package interfaces
 
 import "fmt"
 
-func do(i interface{}) {
-	switch v := i.(type) {
-	case int:
-		fmt.Printf("Twice %v is %v\n", v, v*2)
-	case string:
-		fmt.Printf("%q is %v bytes long\n", v, len(v))
-	default:
-		fmt.Printf("I don't know about type %T!\n", v)
-	}
-}
-
+// empty interface{} is like dynamic typing in Python (or 'any' in new Go)
 func TypeAssertion() {
-	do(21)
-	do("hello")
-	do(true)
+	var x interface{} = 42
+	fmt.Println("value of any type:", x)
 }
