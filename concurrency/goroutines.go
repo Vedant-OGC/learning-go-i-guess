b8fa18117436d6d1e7fd67dd4a4944e3ee308907
Vedant-OGC <sharonverma73@gmail.com>
2026-05-15 15:50:00 +0530

    atomic operations

--- concurrency/goroutines.go
+++ concurrency/goroutines.go
@@ -3,20 +3,20 @@ package concurrency
 import (
 	"fmt"
 	"sync"
+	"sync/atomic"
 )
 
 func Goroutines() {
-	// race condition!
-	var counter int
+	var counter int64
 	var wg sync.WaitGroup
 	
 	for i := 0; i < 1000; i++ {
 		wg.Add(1)
 		go func() {
-			counter++ // concurrent write, race condition!
+			atomic.AddInt64(&counter, 1) // atomic operation
 			wg.Done()
 		}()
 	}
 	wg.Wait()
-	fmt.Println("Counter (racy):", counter)
+	fmt.Println("Counter (atomic, safe):", counter)
 }
