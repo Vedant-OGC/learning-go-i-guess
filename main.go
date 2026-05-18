bdd599ab3425e24d3b0a41ace77a8eda2e48c05d
Vedant-OGC <sharonverma73@gmail.com>
2026-05-18 15:10:00 +0530

    custom packages import

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/Vedant-OGC/learning-go-i-guess/packages"
 )
 
 func main() {
-	fmt.Println("day 6: functions completed!")
-	fmt.Println("doing some basic operations.")
+	fmt.Println("testing my packages!")
+	fmt.Println("5 + 3 =", packages.Add(5, 3))
+	fmt.Println("reverse 'hello':", packages.Reverse("hello"))
 }
