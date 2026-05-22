a14a633315db5f47d0312c09c32015c596e537de
Vedant-OGC <sharonverma73@gmail.com>
2026-05-22 12:50:00 +0530

    todo cli storage file persistence

---
+++ mini_projects/todo_cli/storage.go
@@ -0,0 +1,24 @@
+package todo_cli
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func Save(path string, list *TodoList) error {
+	data, err := json.Marshal(list)
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
+
+func Load(path string) (*TodoList, error) {
+	var list TodoList
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return &list, err
+	}
+	err = json.Unmarshal(data, &list)
+	return &list, err
+}
