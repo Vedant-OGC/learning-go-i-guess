812606657b222a20bebb8f59a3d65c57221ef0a3
Vedant-OGC <sharonverma73@gmail.com>
2026-05-24 09:30:00 +0530

    url shortener core structs

---
+++ mini_projects/url_shortener/shortener.go
@@ -0,0 +1,33 @@
+package url_shortener
+
+import (
+	"math/rand"
+)
+
+type URLShortener struct {
+	Urls map[string]string
+}
+
+func NewShortener() *URLShortener {
+	return &URLShortener{Urls: make(map[string]string)}
+}
+
+func (s *URLShortener) Shorten(url string) string {
+	code := generateCode()
+	s.Urls[code] = url
+	return code
+}
+
+func (s *URLShortener) Resolve(code string) (string, bool) {
+	url, ok := s.Urls[code]
+	return url, ok
+}
+
+func generateCode() string {
+	letters := []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789")
+	b := make([]rune, 6)
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+	return string(b)
+}
