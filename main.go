0c733f7901816b6563ceb3510c6ca555c526f01e
Vedant-OGC <sharonverma73@gmail.com>
2026-05-25 11:00:00 +0530

    integrating expense_tracker into main

--- main.go
+++ main.go
@@ -3,10 +3,11 @@ package main
 import (
 	"fmt"
 	"github.com/Vedant-OGC/learning-go-i-guess/mini_projects/todo_cli"
+	"github.com/Vedant-OGC/learning-go-i-guess/mini_projects/expense_tracker"
 )
 
 func main() {
-	fmt.Println("welcome to learning go launcher")
-	fmt.Println("integrating todo cli...")
+	fmt.Println("launcher: todo and expense tracker integrated")
 	var _ = todo_cli.TodoList{}
+	var _ = expense_tracker.ExpenseBook{}
 }
