2d4c4407ab842ddf8f2efbf78617cbe195a55a60
Vedant-OGC <sharonverma73@gmail.com>
2026-05-25 13:30:00 +0530

    integrating url_shortener into main

--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/Vedant-OGC/learning-go-i-guess/mini_projects/todo_cli"
 	"github.com/Vedant-OGC/learning-go-i-guess/mini_projects/expense_tracker"
+	"github.com/Vedant-OGC/learning-go-i-guess/mini_projects/url_shortener"
 )
 
 func main() {
-	fmt.Println("launcher: todo and expense tracker integrated")
+	fmt.Println("launcher: now with url shortener")
 	var _ = todo_cli.TodoList{}
 	var _ = expense_tracker.ExpenseBook{}
+	var _ = url_shortener.URLShortener{}
 }
